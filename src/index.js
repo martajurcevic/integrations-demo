c1051f5684792884ced9c9165432bb86f909db1e
Marta Jurcevic <marta.raptavi997@gmail.com>
2022-11-09 11:20:34 +0100

    changes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Integrations from './components/pages/Integrations';
+import Integrations from './pages/integrations/Integrations';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
